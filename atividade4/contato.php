9030675dca25df19a225998836700be469f4deaf
ThalitaGuimaraes <102105342+ThalitaGuimaraes@users.noreply.github.com>
2022-05-01 21:50:04 -0300

    Add files via upload

---
+++ atividade4/contato.php
@@ -0,0 +1,9 @@
+<?php
+    $nome= $_POST['txtNome'];
+    $email= $_POST['txtEmail'];
+    $telefone= $_POST['txtPhone'];
+    $msg= $_POST['txtMessage'];
+
+    echo " A pessoa {$nome}, que possui o email: {$email} e o telefone: {$telefone} "
+    ."<br> enviou a mensagem: {$msg} " ;
+?>
